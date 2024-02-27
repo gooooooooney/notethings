9d583c58e913f6e7239eb2bc0ef226c50ef900c0
Wenguang <wenguangzhang@caixin.com>
2024-02-27 13:48:33 +0800

    Update .env.example and app/loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="grid place-items-center animate-pulse text-neutral-300 p-4">
+    <div className="h-screen grid place-items-center animate-pulse text-neutral-300 p-4">
       <div role="status">
         <svg
           aria-hidden="true"
